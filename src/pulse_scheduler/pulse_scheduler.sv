c71291fd22961ec97c810d4fcd1141d635506ae4
Jeremy Mills <jeremymills496@gmail.com>
2025-08-02 21:47:22 -0700

    Comment planning

--- src/pulse_scheduler/pulse_scheduler.sv
+++ src/pulse_scheduler/pulse_scheduler.sv
@@ -13,6 +13,11 @@ module pulse_scheduler (
     // Things we need in the pulse scheduler module:
     // 1. Decode the pulse instruction into frequency, phase, amplitude, t_start, etc.
     // 2. Store that information into the pulse_register
+    // 3. Figure out a way to determine if the pulse is ready to be triggered,
+    //    and if it is, trigger it by sending it to the dac_axi_stream_module
+    //    using cordic, and then lock the pulse register so no other pulses are triggered
+    // 4. Figure out how to expose the clock to the riscv core for qgett instructions
+    // 5. Create an envelope memory to store the envelope data
 
 
 
